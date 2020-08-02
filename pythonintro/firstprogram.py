383ec816e4d1ecf9a0c32a2f428dcb00db7c2ff7
Gauri Prakash Tanaji <prakashtanaji@Tanajis-MacBook-Pro.local>
2020-08-02 15:30:44 -0700

    python intro

---
+++ pythonintro/firstprogram.py
@@ -0,0 +1,24 @@
+#print('macbookpro')
+#print('dheeraj likes apples')
+#print('dheeraj likes ' +  'apples')
+#print('ring')
+
+# Dheeraj 
+animal='shark'
+print('dheeraj likes ' +  animal)
+print('dheeraj likes ' +  animal)
+print('dheeraj likes ' +  animal)
+
+
+
+#Dad
+fruit='mango'
+print('Dad likes ' + fruit )
+print('Dad likes ' +  friut)
+print('Dad likes ' +  fruit)
+print('Dad likes ' +  friut)
+print('Dad likes ' +  friut)
+print('Dad likes ' +  friut)
+print('Dad likes ' +  friut)
+print('Dad likes ' +  friut)
+print('Dad likes ' +  fruit)
---
+++ pythonintro/secondprogram.py
@@ -0,0 +1,8 @@
+person='gauri'
+print(person)
+print('sushma')
+
+for i in range(10):
+    print('dheeraj ',i)
+for j in range(4):
+    print(person)   
